840c1ad267763e9de0e82e442011e70c3d7f5e09
Ana Corina <acorina59@gmail.com>
2023-07-04 11:26:45 -0300

    Starting to implement the classes Board and Piece

--- src/board/Piece.java
+++ src/board/Piece.java
@@ -1,5 +1,14 @@
 package board;
 
 public class Piece {
-    private Position position;
+    protected Position position;
+    private Board board;
+
+    public Piece(Board board) {
+        this.board = board;
+    }
+
+    protected Board getBoard() {
+        return board;
+    }    
 }
